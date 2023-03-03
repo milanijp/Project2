36af505db7d5e52393c7b2525baa12edfcbecb6f
milanijp <124702113+milanijp@users.noreply.github.com>
2023-03-02 22:35:06 -0500

    Create template-processor.js

---
+++ template-processor.js
@@ -0,0 +1,10 @@
+'use strict';
+function TemplateProcessor(template) {
+    this.template = template;
+}
+
+TemplateProcessor.prototype.fillIn = function(dictionary) {
+    var res = this.template;
+    for (var res) {
+    return res;
+}
